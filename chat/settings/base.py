34645dc9bf2a8a50fc3dfd0a8a9e9b34fa677110
swsvc <swsvc@yandex.ru>
2018-03-20 02:25:52 +0300

    parameterize redis host

--- chat/settings/base.py
+++ chat/settings/base.py
@@ -104,7 +104,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            'hosts': [('127.0.0.1', 6379)]
+            'hosts': [(get_env_variable('REDIS_HOST'), 6379)]
         }
     }
 }
